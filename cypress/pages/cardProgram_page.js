3653526332fd22fcc001635e098e1e73e6bed371
Fidelis <fidelissykes@gmail.com>
2024-06-16 13:45:38 +0100

    simulate failed test

--- cypress/pages/cardProgram_page.js
+++ cypress/pages/cardProgram_page.js
@@ -5,7 +5,7 @@ export class CardProgramPage {
         return cy.get(':nth-child(3) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .menu-item-link > .menu-item')
     }
     cardProgramNameInput() {
-        return cy.get('.form-control')
+        return cy.get('.form-control--')
     }
     returnCardProgramUrl() {
         return cy.url()
